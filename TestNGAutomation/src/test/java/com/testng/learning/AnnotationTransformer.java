f4515773341947abe0d9ad508a074227aa112ab6
sreeram <sreeram@DESKTOP-ICG6BLL>
2020-12-12 23:14:24 +0530

    Modified AnnotationTransformer class

--- TestNGAutomation/src/test/java/com/testng/learning/AnnotationTransformer.java
+++ TestNGAutomation/src/test/java/com/testng/learning/AnnotationTransformer.java
@@ -8,7 +8,6 @@ import org.testng.annotations.ITestAnnotation;
 
 public class AnnotationTransformer implements IAnnotationTransformer{
 
-	//Transform method
 	public  void transform(ITestAnnotation annotation,  Class testClass, Constructor testConstructor, Method testMethod) {
 		annotation.setRetryAnalyzer(RetryAnalyzer.class);
 	  }
