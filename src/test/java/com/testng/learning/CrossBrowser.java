f78a3116805c4c1db8c68a0e75295d3d30df92f8
Sreeram <sreerambeece@gmail.com>
2021-10-15 16:16:26 +0530

    Selenium 4 and test ng version updated

--- src/test/java/com/testng/learning/CrossBrowser.java
+++ src/test/java/com/testng/learning/CrossBrowser.java
@@ -1,64 +1,49 @@
 package com.testng.learning;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.util.Objects;
+
 
 public class CrossBrowser {
 
-	public WebDriver driver;
-	String baseURL="https://www.google.com";
-	
-	@BeforeTest
-	@Parameters("browser")
-	public void browser_Selection(String browser) throws Exception {
-		if(browser.equalsIgnoreCase("Chrome")) {
-			System.setProperty("webdriver.chrome.driver", "C:\\Dev\\chromedriver.exe");
-			driver = new ChromeDriver();
-		}
-		else if(browser.equalsIgnoreCase("Firefox")){
-			System.setProperty("webdriver.gecko.driver", "C:\\Dev\\geckodriver.exe");
-			driver = new FirefoxDriver();
-		}
-		else if(browser.equalsIgnoreCase("Edge")){
-			System.setProperty("webdriver.edge.driver", "C:\\Dev\\msedgedriver.exe");
-			driver = new EdgeDriver();
-		}
-		else {
-			throw new Exception("Other browser defined");
-		}
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-	
-	@Test
-	public void launchURL() {
-		driver.get(baseURL);
-		String expectedTitle="Google";
-		String actualTitle=driver.getTitle();
-		Assert.assertEquals(expectedTitle, actualTitle);
-	}
+    public WebDriver driver;
+    String baseURL = "https://www.google.com";
 
-	@AfterTest
-	@Parameters("browser")
-	public void killDriver(String browser) throws IOException {
-		driver.close();
-		if(browser.equalsIgnoreCase("Chrome")) {
-		Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
-		}
-		else if(browser.equalsIgnoreCase("Firefox")){
-		Runtime.getRuntime().exec("taskkill /F /IM geckodriver.exe /T");
-		}
-		else if(browser.equalsIgnoreCase("Edge")){
-		Runtime.getRuntime().exec("taskkill /F /IM msedgedriver.exe /T");
-		}
-	}
+    @BeforeTest
+    @Parameters("browser")
+    public void browser_Selection(String browser) throws Exception {
+        if (browser.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            throw new Exception("Other browser defined");
+        }
+    }
+
+    @Test
+    public void launchURL() {
+        driver.get(baseURL);
+        String expectedTitle = "Google";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+    }
+
+    @AfterTest
+    public void killDriver() {
+        if (Objects.nonNull(driver)) {
+            driver.quit();
+        }
+    }
 }
