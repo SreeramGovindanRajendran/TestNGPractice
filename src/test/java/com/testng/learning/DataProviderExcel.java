18021bd70976030008aea3f32068f56c6aa1b57a
Sreeram <sreerambeece@gmail.com>
2022-01-28 15:59:24 +0530

    Update DataProvider

--- src/test/java/com/testng/learning/DataProviderExcel.java
+++ src/test/java/com/testng/learning/DataProviderExcel.java
@@ -8,44 +8,19 @@ import org.testng.annotations.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class DataProviderExcel {
 
+    /*
+need to declare all param from excel when using object[][] data provider
+ */
+
     @Test(dataProvider = "excelData1")
-    public void test1(String user, String name, String password ){
-        System.out.println(user +" "+ name+ " "+password);
+    public void test1(String user, String name, String password) {
+        System.out.println(user + " " + name + " " + password);
     }
 
-    /*
-    returning the map for all the input parameters - more memory
-     */
-    @DataProvider
-    public Object[][] excelData() throws IOException {
-        FileInputStream fis = new FileInputStream("C:\\Dev\\workspace\\TestNGPractice\\src\\test\\resources\\tmb\\data.xlsx");
-        XSSFWorkbook wb = new XSSFWorkbook(fis);
-        XSSFSheet sheet = wb.getSheet("Sheet1");
-
-        int rowNum = sheet.getLastRowNum();
-        int colNum = sheet.getRow(0).getLastCellNum();
-
-        Object[][] data = new Object[rowNum][colNum];
-        LinkedHashMap<String,String> map;
-
-        for(int i=1;i<=rowNum;i++){
-            map = new LinkedHashMap<>();
-            for(int j=0;j<colNum;j++){
-                String key = sheet.getRow(0).getCell(j).getStringCellValue();
-                String value = sheet.getRow(i).getCell(j).getStringCellValue();
-                map.put(key,value);
-                data[i-1][j]=map;
-            }
-        }
-        return data;
-    }
-
-    /*
-   returning only the value for respective input parameters - less memory
-    */
 
     @DataProvider
     public Object[][] excelData1() throws IOException {
@@ -58,10 +33,48 @@ public class DataProviderExcel {
 
         Object[][] data = new Object[rowNum][colNum];
 
-        for(int i=1;i<=rowNum;i++){
-            for(int j=0;j<colNum;j++){
+        for (int i = 1; i <= rowNum; i++) {
+            for (int j = 0; j < colNum; j++) {
                 String value = sheet.getRow(i).getCell(j).getStringCellValue();
-                data[i-1][j]=value;
+                data[i - 1][j] = value;
+            }
+        }
+        return data;
+    }
+
+    /*
+only declare map when using object[] data provider
+ */
+    @Test(dataProvider = "excelData")
+    public void test2(Map<String, String> map) {
+        System.out.println(map.get("user"));
+        System.out.println(map.get("name"));
+        System.out.println(map.get("password"));
+    }
+
+
+    /*
+    returning the map for all the input parameters - more memory
+     */
+    @DataProvider
+    public Object[] excelData() throws IOException {
+        FileInputStream fis = new FileInputStream("C:\\Dev\\workspace\\TestNGPractice\\src\\test\\resources\\tmb\\data.xlsx");
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        XSSFSheet sheet = wb.getSheet("Sheet1");
+
+        int rowNum = sheet.getLastRowNum();
+        int colNum = sheet.getRow(0).getLastCellNum();
+
+        Object[] data = new Object[rowNum];
+        Map<String, String> maps;
+
+        for (int i = 1; i <= rowNum; i++) {
+            maps = new LinkedHashMap<>();
+            for (int j = 0; j < colNum; j++) {
+                String key = sheet.getRow(0).getCell(j).getStringCellValue();
+                String value = sheet.getRow(i).getCell(j).getStringCellValue();
+                maps.put(key, value);
+                data[i - 1] = maps;
             }
         }
         return data;
